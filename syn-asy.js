4e2170a67f722ca5b7dfa1ab506791b3f899bb92
Hashim. A <162900023+HashimAyamon@users.noreply.github.com>
2025-07-29 15:18:45 +0530

    2nd commit

---
+++ syn-asy.js
@@ -0,0 +1,14 @@
+// syn 
+console.log("Step 1");
+console.log("Step 2");
+console.log("Step 3");
+
+
+
+
+// asy 
+console.log("Start");
+setTimeout(() => {
+  console.log("Async task done");
+}, 2000);
+console.log("End");
--- typeConversion.js
+++ typeConversion.js
@@ -1,9 +1,9 @@
-console.log("5" + 2);     // "52" → number 2 is converted to string
+console.log("5" + 2); 
 
-console.log("10" - 3);    // 7   → string "10" is converted to number
+console.log("10" - 3);  
 
-console.log(true + 1);    // 2   → true becomes 1
+console.log(true + 1);    
 
-console.log(null + 1);    // 1   → null becomes 0
+console.log(null + 1);    
 
-console.log("abc" * 2);   // NaN → "abc" can't be converted to a number
+console.log("abc" * 2); 
